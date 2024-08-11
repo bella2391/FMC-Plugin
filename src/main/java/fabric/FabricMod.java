4f50a36e5bbfeacd301611b4271ed1846e987a52
bella <maekawatakaya10.8@gmail.com>
2024-08-12 06:51:37 +0900

    update

---
+++ src/main/java/fabric/FabricMod.java
@@ -0,0 +1,10 @@
+package fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello, Fabric world!");
+    }
+}
