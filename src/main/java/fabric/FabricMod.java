fbd9666ca0f3dddaee1ad2ad54e161ef4f0ae905
bella <maekawatakaya10.8@gmail.com>
2024-08-12 20:00:38 +0900

    update

--- src/main/java/fabric/FabricMod.java
+++ src/main/java/fabric/FabricMod.java
@@ -1,28 +1,39 @@
 package fabric;
 
 import org.slf4j.Logger;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
-@SpringBootApplication
 public class FabricMod implements ModInitializer
 {
-	private FabricLoader fabric;
-	private Logger logger;
+	private static Injector injector = null;
+	private final FabricLoader fabric;
+	private final Logger logger;
 	private Config config;
 	
-	@Override
+	public FabricMod()
+	{
+		this.fabric = FabricLoader.getInstance();
+		this.logger = LoggerFactory.getLogger("FMC");
+	}
+	
+    @Override
     public void onInitialize()
     {
-		ApplicationContext context = SpringApplication.run(FabricApplication.class);
-		this.config = context.getBean(Config.class);
-        this.logger = context.getBean(Logger.class);
-		this.fabric = context.getBean(FabricLoader.class);
         System.out.println("Hello, Fabric world!");
+        injector = Guice.createInjector(new FabricModule(fabric, logger));
         
+        this.config = getInjector().getInstance(Config.class);
         logger.info(config.getString("MySQL.Host"));
+    }
+    
+    public static Injector getInjector()
+    {
+        return injector;
     }
 }
